28104b2053080917672da22ab4032d253c3dc574
Alexander <alexandrsuchkov67@gmail.com>
2021-12-10 12:01:01 +0300

    updated services

--- backend/app/service/schema.go
+++ backend/app/service/schema.go
@@ -1,8 +1,77 @@
 package service
 
-type SchemaService struct{}
+import (
+	"skema/app/model"
+	"skema/app/repo"
+	"skema/util"
+)
 
-func NewSchemaService() *SchemaService {
-	service := new(SchemaService)
-	return service
+type SchemaService struct {
+	schemaRepo  repo.ISchemaRepo
+	commentRepo repo.ICommentRepo
+}
+
+func NewSchemaService(schemaRepo repo.ISchemaRepo, commentRepo repo.ICommentRepo) *SchemaService {
+	serv := SchemaService{schemaRepo, commentRepo}
+	return &serv
+}
+
+func (s *SchemaService) CreateSchema(data *model.Schema) error {
+	err := s.schemaRepo.Create(data)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "SchemaService", "CreateSchema")
+	return err
+}
+
+func (s *SchemaService) TakeSchemaById(id uint) (*model.Schema, error) {
+	schema, err := s.schemaRepo.TakeOneById(id)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "SchemaService", "TakeSchemaById")
+	return schema, err
+}
+
+func (s *SchemaService) TakeUserSchemes(owner uint) (*[]model.Schema, error) {
+	schemes, err := s.schemaRepo.FilterByOwner(owner)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "SchemaService", "TakeUserSchemes")
+	return schemes, err
+}
+
+func (s *SchemaService) TakeCommentsByUserId(id uint) (*[]model.Comment, error) {
+	comments, err := s.commentRepo.TakeByUserId(id)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "SchemaService", "TakeCommentsByUserId")
+	return comments, err
+}
+
+func (s *SchemaService) TakeSchemaCommentByUserId(schemaId uint, userId uint) (*[]model.Comment, error) {
+	comments, err := s.commentRepo.FilterBySchemaAndUserId(schemaId, userId)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "SchemaService", "TakeSchemaCommentByUserId")
+	return comments, err
 }
--- backend/app/service/user.go
+++ backend/app/service/user.go
@@ -3,11 +3,14 @@ package service
 import (
 	"skema/app/model"
 	"skema/app/repo"
+	"skema/util"
 )
 
 type UserService struct {
 	repo repo.IUserRepo
 }
+
+// var infoUserLogger = util.NewInfoLogger()
 
 func NewUserService(repo repo.IUserRepo) *UserService {
 	serv := UserService{repo}
@@ -15,17 +18,37 @@ func NewUserService(repo repo.IUserRepo) *UserService {
 }
 
 func (s *UserService) CreateUser(data *model.User) error {
-	return s.repo.Create(data)
+	err := s.repo.Create(data)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "UserService", "CreateUser")
+	return err
 }
 
 func (s *UserService) TakeUserById(id uint) (*model.User, error) {
 	user, err := s.repo.TakeOneById(id)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "UserService", "TakeUserById")
 	return user, err
 }
 
 func (s *UserService) TakeUserRatingMoreThan(rating int) (*[]model.User, error) {
 	users, err := s.repo.TakeByRatingMoreThan(rating)
+	status := "Success"
+
+	if err != nil {
+		status = "Error"
+	}
+
+	util.Logger.WriteInfoLog(status, "UserService", "TakeUserRatingMoreThan")
 	return users, err
 }
-
-// mockery -dir ./repo/ -all
