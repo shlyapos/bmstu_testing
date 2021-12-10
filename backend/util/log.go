daf8afb5bd6e954d49a289ba441c8dc80ae3607c
Alexander <alexandrsuchkov67@gmail.com>
2021-12-10 11:59:16 +0300

    added logger

---
+++ backend/util/log.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"log"
+	"os"
+)
+
+type InfoLogger struct {
+	logFileInfo *os.File
+	logger      *log.Logger
+}
+
+func NewInfoLogger() *InfoLogger {
+	infoLogger := new(InfoLogger)
+
+	f, err := os.OpenFile("../../logs/info.log", os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infoLogger.logFileInfo = f
+	infoLogger.logger = log.New(f, "[INFO]\t", log.Ldate|log.Ltime)
+
+	return infoLogger
+}
+
+func (l *InfoLogger) WriteInfoLog(status string, className string, method string) {
+	l.logger.Printf("\t[STATUS] %v  \t[CLASS] %v\t[METHOD] %v", status, className, method)
+}
+
+func (l *InfoLogger) CloseAllLogFiles() {
+	l.logFileInfo.Close()
+}
+
+var Logger = NewInfoLogger()
